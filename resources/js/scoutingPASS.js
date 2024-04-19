ceb453135f696ebb1a94ae2f5d6833c5eb5cecb8
pdhopkinsrobotics <107653146+pdhopkinsrobotics@users.noreply.github.com>
2024-04-19 15:54:40 -0500

    adding debug info, moving input formatting to CSS

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -179,7 +179,7 @@ function addCounter(table, idx, name, data) {
   } else {
     inp.setAttribute("name", data.code);
   }
-  inp.setAttribute("style", "background-color: black; color: white;border: none; text-align: center;");
+  inp.setAttribute("style", "text-align: center;");
   inp.setAttribute("disabled", "");
   inp.setAttribute("value", 0);
   inp.setAttribute("size", 2);
